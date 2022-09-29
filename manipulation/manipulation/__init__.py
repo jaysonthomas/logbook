17bd53b49f8d5369f10cc024984437afe0704c75
Jayson Thomas <jayson.j.thomas@gmail.com>
2022-09-29 18:31:14 +0100

    Currently working on forceControl.html Added some diy content related to blinds. Started on the ICP (with the bunnies) but decided to focus on forceControl instead for now. Created pages for fundamentals and randomTerms. Deleted all the unnecessary stuff from the manipulation lectures.

--- manipulation/manipulation/__init__.py
+++
@@ -1,3 +0,0 @@
-# Note: don't import running_as_test here; it will give a copy
-# instead of a reference.
-from .utils import FindResource, running_as_notebook
--- manipulation/manipulation/test/test_utils.py
+++
@@ -1,5 +0,0 @@
-from manipulation.utils import SystemHtml
-from manipulation.scenarios import MakeManipulationStation
-
-station = MakeManipulationStation()
-SystemHtml(station)
