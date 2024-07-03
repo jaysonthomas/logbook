407474c1d7bb4ed88efa168d03924bf416dff594
Jayson Thomas <jayson.j.thomas@gmail.com>
2024-07-03 20:41:11 +0100

    Started cleaning up estimation. Finished section on 2-mass single spring. Did some writing on Taylor Series.

--- logbook-mathjax-config.js
+++ logbook-mathjax-config.js
@@ -1,16 +1,26 @@
 window.MathJax = {
   startup: {
+    // Ensures that MathJax doesn't automatically typeset the document when it loads. Typesetting needs to be manually triggered later using a js script.
     typeset: false
   },
   tex: {
+    // Delimiters for inline and display math.
     inlineMath: [ ['$','$'], ["\\(","\\)"] ],
     displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+
+    // Allows the use of backslash escapes in the text.
     processEscapes: true,
+
+    // Enables automatic equation numbering in the AMS style.
     tags: 'ams',
-    packages: {'[+]': ['cancel']}
+
+    // Includes the cancel and color packages.
+    packages: {'[+]': ['cancel', 'color']}
   },
   svg: {
+    // Ensures that the font cache is shared across the document.
     fontCache: 'global'
   },
-  loader: {load: ['[tex]/cancel']},
+  // Ensures that the required packages are loaded.
+  loader: {load: ['[tex]/cancel', '[tex]/color']},
 };
